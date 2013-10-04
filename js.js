556fa74534cdc8ef0c61a314ef5d8dce564aae70
dmytro <dmytro@pixelant.se>
2013-10-04 18:26:47 +0300

    six slides

--- js.js
+++ js.js
@@ -1,7 +1,15 @@
+(function($,undefined){
+
 $(function(){
 
+var ddd = window.innerHeight;
+console.log(ddd);
+var ttt = ddd /10 + 'rem'
+console.log(ddd);
+$('.slide-height').css('height', ttt);
 
 });
 
+}(jQuery));
 
 
